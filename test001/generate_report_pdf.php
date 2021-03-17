d87bb483293e619b0fab5c1181c744f4113473b2
Amin Ledang @ Dell <helmi007@yahoo.com>
2021-03-17 23:13:21 +0800

    buat line baru

--- test001/generate_report_pdf.php
+++ test001/generate_report_pdf.php
@@ -2,20 +2,21 @@
 #--------------------------------------------------------------------------------------------------
 # include fail2 yang penting
 require 'fpdf182/fpdf.php';
-require 'class_fpdfv01.php';
+//require 'class_fpdfv01.php';
 require 'tatarajah.php';
 require 'Database.php';
 include 'patient.php';
 #--------------------------------------------------------------------------------------------------
-# mula panggil data dari sql
+/*# mula panggil data dari sql
 $id=isset($_GET['id_patient']) ? $_GET['id_patient'] : die('ERROR: Record ID not found.');
 $body1=new Database();
 $link=$body1->MyDatabase();
 $data=new patient($link);
-$row = $data->ReadOneV02($id);
+$row = $data->ReadOneV02($id);*/
 #--------------------------------------------------------------------------------------------------
 # mula panggil fail pdf
-$pdf = new FPDF();
+//$pdf = new FPDF();
+$pdf = new FPDF( 'P', 'mm', 'A4' ); // A4, portrait, measurements in mm.
 #--------------------------------------------------------------------------------------------------
 # setkan pembolehubah berdasarkan $pdf
 $pdf->AddPage();
@@ -23,8 +24,10 @@ $pdf->SetFont('Arial','B',16);
 # papar tajuk utama
 //$pdf->Cell(40,10,'Hello World!');
 $pdf->Cell(40,10,'Patient Details');
-// Thickness of frame (1 mm)
-$this->SetLineWidth(5);
+# buat line
+$pdf->SetDrawColor(188,188,188);
+$pdf->Line(20, 45, 210-20, 45); // 20mm from each edge
+$pdf->Line(50, 45, 210-50, 45); // 50mm from each edge
 #--------------------------------------------------------------------------------------------------
 # papar output last
 $pdf->Output();
